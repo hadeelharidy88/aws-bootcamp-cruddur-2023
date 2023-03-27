cc611f047e9c0bdfaf595b7d479aaed87c7a6f53
hadeelharidy88 <hadeel.haridy.88@gmail.com>
2023-03-27 21:25:48 +0000

    instrument Rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,6 +14,12 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# Rollbar----------------
+
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 
 # Cloudwatch logs------------------
 import watchtower
@@ -51,6 +57,7 @@ from opentelemetry.sdk.trace.export import SimpleSpanProcessor, ConsoleSpanExpor
 
 
 
+
 # Honeycomb-------------------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -64,6 +71,8 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+
 
 # xray---------------------
 #XRayMiddleware(app, xray_recorder)
@@ -86,12 +95,40 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
+
+# Rollbar----------------
+
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
 # Cloudwatch logs---------------------------
 #@app.after_request
 #def after_request(response):
  #   timestamp = strftime('[%Y-%b-%d %H:%M]')
   #  LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
    # return response
+
+# Rollbar----------------
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
